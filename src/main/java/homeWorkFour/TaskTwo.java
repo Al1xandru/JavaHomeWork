503c902c10823c992d8baf8d442a5243bb2f887c
Alex <al1xbodeanu@gmail.com>
2024-07-10 21:03:53 +0200

    TaskTwo

--- src/main/java/homeWorkFour/TaskTwo.java
+++ src/main/java/homeWorkFour/TaskTwo.java
@@ -1,5 +1,32 @@
 package homeWorkFour;
 
+import java.util.Random;
+
 public class TaskTwo {
 
+    final static int taskNumber = new Random().nextInt(28801);
+
+    public static void main(String[] args) {
+        System.out.println(taskNumber + " seconds left");
+        int hourLeft = (taskNumber / 60) / 60;
+        switch (hourLeft){
+            case 1:
+                System.out.println("осталось " + hourLeft + " час");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("осталось " + hourLeft + " часа");
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("осталось " + hourLeft + " часов");
+                break;
+            default:
+                System.out.println("осталось менее часа");
+        }
+
+    }
 }
