4c8381607f4ba7abd7e9bb8eade36e9a1e28a169
Alex <al1xbodeanu@gmail.com>
2024-07-10 18:20:25 +0200

    TaskTwo

--- src/main/java/homeWorkThree/TaskTwo.java
+++ src/main/java/homeWorkThree/TaskTwo.java
@@ -11,10 +11,6 @@ public class TaskTwo {
         return remakeStringToReverse(string.toUpperCase());
     }
     public static String remakeStringToReverse(String string){
-        String resultString = "";
-        for(int i = string.length() - 1; i >= 0; i--){
-            resultString += string.charAt(i);
-        }
-        return resultString;
+        return new StringBuilder(string).reverse().toString();
     }
 }
